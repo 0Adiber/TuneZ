9ee92d5eb7d4ed1be79a3f682bd09caa70f1faa5
Adiber <adiber.yt@gmail.com>
2019-09-21 17:20:02 +0200

    11.4
    oops

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Bot = require("./Bot/index.js").bot;
 require('dotenv').config()
 
-if(process.env.TEST) {
+if(process.env.TEST === "true") {
+    console.log("test");
     Bot(process.env.TEST_TOKEN, "1", "!", "#00ff00");
 } else {
     Bot(process.env.BOT_TOKEN, "1", "!", "#42f2f5");
